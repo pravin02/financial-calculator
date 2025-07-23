c97e938df4bb8e65997050b38e1d85ba1112ec83
Pravin <prvnpatil11@gmail.com>
2025-07-23 18:10:57 +0530

    primereact removed and custom html css sidemenu implemented

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,94 @@ function App() {
 
   return (
     <>
-      <div className='sidebar'>
+      <nav>
+        <ul className="mcd-menu">
+          <li>
+            <a href="">
+              <i className="fa fa-home"></i>
+              <strong>Financial Calendar</strong>
+              <small>A companion Calculator</small>
+            </a>
+          </li>
+          <li className="float">
+            <a className="search">
+              <input type="text" value="search ..." />
+              <button><i className="fa fa-search"></i></button>
+            </a>
+            <a href="" className="search-mobile">
+              <i className="fa fa-search"></i>
+            </a>
+          </li>
+          <li>
+            <a href="" className="active">
+              <i className="fa fa-edit"></i>
+              <strong>About us</strong>
+              <small>sweet home</small>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="fa fa-gift"></i>
+              <strong>Features</strong>
+              <small>sweet home</small>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="fa fa-globe"></i>
+              <strong>News</strong>
+              <small>sweet home</small>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="fa fa-comments-o"></i>
+              <strong>Blog</strong>
+              <small>what they say</small>
+            </a>
+            <ul>
+              <li><a href="#"><i className="fa fa-globe"></i>Mission</a></li>
+              <li>
+                <a href="#"><i className="fa fa-group"></i>Our Team</a>
+                <ul>
+                  <li><a href="#"><i className="fa fa-female"></i>Leyla Sparks</a></li>
+                  <li>
+                    <a href="#"><i className="fa fa-male"></i>Gleb Ismailov</a>
+                    <ul>
+                      <li><a href="#"><i className="fa fa-leaf"></i>About</a></li>
+                      <li><a href="#"><i className="fa fa-tasks"></i>Skills</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#"><i className="fa fa-female"></i>Viktoria Gibbers</a></li>
+                </ul>
+              </li>
+              <li><a href="#"><i className="fa fa-trophy"></i>Rewards</a></li>
+              <li><a href="#"><i className="fa fa-certificate"></i>Certificates</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="">
+              <i className="fa fa-picture-o"></i>
+              <strong>Portfolio</strong>
+              <small>sweet home</small>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <i className="fa fa-envelope-o"></i>
+              <strong>Contacts</strong>
+              <small>drop a line</small>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <section>
+        sdsdsada
+      </section>
+      {/* <div classNameName='sidebar'>
         <FinCalMenu />
       </div>
-      <div className='content'>
+      <div classNameName='content'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -25,7 +109,7 @@ function App() {
           <Route path="/mf/sip" element={<SystematicInvestmentPlan />} />
           <Route path="/mf/swp" element={<SystematicWithdrawalPlan />} />
         </Routes>
-      </div>
+      </div> */}
     </>
   )
 }
--- src/mutualfunds/ells.jsx
+++ src/mutualfunds/ells.jsx
@@ -1,40 +1,10 @@
 import React from 'react';
 
-import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
-import { ButtonGroup } from 'primereact/buttongroup';
-import { FloatLabel } from 'primereact/floatlabel';
-import { InputText } from 'primereact/inputtext';
-import { Divider } from 'primereact/divider';
-
 
 function EquityLinkedSavingScheme() {
 
     return (
-        <Card title="Equity Linked Savings Scheme">
-            <div className='grid columns-2'>
-                <label htmlFor='depositAmount'> Lump sum Deposit Amount </label>
-                <InputText id="depositAmount" keyfilter="int" raised />
-            </div>
-            <div className='grid columns-2'>
-                <label htmlFor='expectedReturnPercentage'> Expected Returns Rate (%) </label>
-                <InputText id="expectedReturnPercentage" />
-            </div>
-            <div className='grid'>
-                <label> 3 Years </label>
-                <label> Lockin Term </label>
-
-            </div>
-            <div className='grid'>
-                <label htmlFor='depositTerm'> Deposit Term In Years</label>
-                <InputText id="depositTerm" alt='3 years or more' />
-            </div>
-            <Divider />
-            <ButtonGroup>
-                <Button label='Reset' raised icon="pi pi-times" />
-                <Button label='Calculate' raised icon="pi pi-check" />
-            </ButtonGroup>
-        </Card>
+        <>ELLS</>
     )
 
 }
